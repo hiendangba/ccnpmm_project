e2b15d633283060947d52b763d761ad1951bc131
phamhoanghuy2004 <phamhoanghuy.2000vn@gmail.com>
2025-09-26 22:26:33 +0700

    Huy update share

--- src/routes/client/post.route.js
+++ src/routes/client/post.route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const route = express.Router();
 const { authMiddleware } = require("../../middlewares/auth.middleware");
-const { loadListPostMiddleware, likePostMiddleware, commentPostMiddleware, handleImages } = require("../../middlewares/post.middleware");
+const { loadListPostMiddleware, likePostMiddleware, commentPostMiddleware, handleImages, sharePostMiddleware } = require("../../middlewares/post.middleware");
 const postController = require("../../controllers/post.controller");
 const upload = require("../../middlewares/upload.middleware"); // multer memoryStorage
 
 route.get("/getAll", authMiddleware, loadListPostMiddleware,  postController.getAllPost);
 route.post("/likePost", authMiddleware, likePostMiddleware , postController.likePost);
 route.post("/commentPost", authMiddleware, upload.array("images") , handleImages, commentPostMiddleware, postController.commentPost );
+route.post("/sharePost", authMiddleware, sharePostMiddleware, postController.sharePost  );
 
 module.exports = route;
